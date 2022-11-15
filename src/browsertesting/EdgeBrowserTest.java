78a0a539e0082cfd21e8f069d3fc8dcf4c290429
hassi665 <hassi665@gmail.com>
2022-11-15 13:02:51 +0000

    Selenium homework week 11 harshid project 2

---
+++ src/browsertesting/EdgeBrowserTest.java
@@ -0,0 +1,46 @@
+package browsertesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
+public class EdgeBrowserTest {
+    public static void main(String[] args) {
+
+        String baseURL = "https://opensource-demo.orangehrmlive.com/";
+        System.setProperty("webdriver.msedge.driver","drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+
+        //launch the URL in EdgeBrowser
+        driver.get(baseURL);
+        //maximize the browser's window
+        driver.manage().window().maximize();
+        //give implicit time to driver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+
+        //Print the title of the page
+        System.out.println("Page title is : " + driver.getTitle());
+
+        //Print the current URL
+        System.out.println("Current URL of page is : " + driver.getCurrentUrl());
+
+        //Print the Page Source
+        System.out.println("Page Source : " + driver.getPageSource());
+
+        //Enter the Username to Username field
+        WebElement usernameField = driver.findElement(By.name("username"));
+        usernameField.sendKeys("Admin");
+
+
+        //Enter the Password to Password field
+        WebElement passwordField = driver.findElement(By.name("password"));
+        passwordField.sendKeys("admin123");
+
+        //Close the browser
+        driver.close();
+
+    }
+}
